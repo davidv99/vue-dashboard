2ce87f752caaefdb844f4801fc38f1fdabd97a35
David Valbuena <david.valbuena@evertecinc.com>
2020-10-19 20:21:00 -0500

    added tailwind dependencie and scaffolding

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+    './public/**/*.html',
+    './src/**/*.vue'
+  ],
+  theme: {
+    colors: {
+      'awesome-color': '#91fa2f'
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
